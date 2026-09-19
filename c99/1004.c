b60ec55838cbb4c7ec2d45f2217b5601f80be120
David Santana <getdavid00@gmail.com>
2026-09-19 01:16:29 -0300

    Exercicios BeeCrowd C99

---
+++ c99/1004.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int a, b, prod;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    prod = a * b;
+
+    printf("PROD = %d\n", prod);
+
+    return 0;
+
+}
